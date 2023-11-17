823515dc19080b14c0ec82252e2f8b1b905d1f92
Riccardo Merli <riccardo.merli017@gmail.com>
2023-11-17 15:04:23 +0100

    mod exercise

--- src/Test5/Test5test.java
+++ src/Test5/Test5test.java
@@ -3,6 +3,8 @@ package Test5;
 import Date4.DateFormat;
 import org.junit.jupiter.api.Test;
 
+import java.time.OffsetDateTime;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -10,6 +12,14 @@ public class Test5test {
     String testString = "2023-03-01T13:00:00Z";
 
     DateFormat date = new DateFormat();
+
+
+    @Test
+    void dataParse() {
+        OffsetDateTime expected = OffsetDateTime.parse(testString);
+        OffsetDateTime output = date.parsing(testString);
+        assertEquals(expected, output);
+    }
 
     @Test
     void dataAddYear() {
